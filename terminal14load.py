f0003bd0e1b8fee001b6298c1265bf597ff4a80b
mahegiogetiluciani-stack <mahegiogetiluciani@gmail.com>
2026-03-16 08:11:15 +0100

    Create terminal14load.py

---
+++ terminal14load.py
@@ -0,0 +1,11 @@
+import time
+import terminal14
+
+def installation():
+	a = input("ceci est la procedure d'installation de chaos")
+	if a == "arch install chaos" or "arch install chaos1.4":()
+	
+	else:
+		print("inconnue")
+		time.sleep(2)
+		installation()
